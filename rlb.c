39aaab6d8373cc9a6573a1ec561b092faf69d5f8
Jason Armstrong <ja@riverdrums.com>
2006-03-18 13:22:18 +0000

    No frills round-robin load balancer.

--- rlb.c
+++ rlb.c
@@ -1,3 +1,4 @@
+/* rlb.c © RIVERDRUMS 2006 $Id$ */
 #include <time.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -20,31 +21,31 @@
 
 struct server {
   int weight, status, num, max;
-  unsigned long long tot;
   struct addrinfo *ai;
   time_t last;
-};
-
-struct cfg {
-  int bufsize, si, cs, num, daemon, fd, check, max;
-  struct connection *conn;
-  struct server *servers;
-  struct timeval to;
-  char host[64], port[8];
-  struct sockaddr oaddr;
-  size_t olen;
-  pid_t pid;
 };
 
 struct connection {
   int c, s;
   char *rb, *wb;
-  size_t rlen, rpos, rsize, wlen, wpos, wsize;
+  size_t rlen, rpos, wlen, wpos;
   struct event c_rev, c_wev, s_rev, s_wev;
   struct server *server;
   struct cfg *cfg;
 };
 
+struct cfg {
+  int bufsize, si, cs, num, daemon, fd, check, max, ci;
+  struct connection *conn;
+  struct server *servers;
+  struct client *clients;
+  struct timeval to;
+  char host[64], port[8];
+  struct sockaddr oaddr;
+  size_t olen;
+};
+
+static struct server * _get_server(struct cfg *cfg, struct connection *c);
 struct addrinfo * _get_addrinfo(struct cfg *cfg, char *h, char *p);
 static int  _cmdline(struct cfg *cfg, int argc, char *argv[]);
 static void _check_server(struct cfg *cfg, struct server *s);
@@ -53,16 +54,13 @@ static void _write(const int fd, short event, void *c);
 static int  _lookup_oaddr(struct cfg *cfg, char *outb);
 static void _read(const int fd, short event, void *c);
 static int  _parse_server(struct cfg *cfg, char *str);
-static struct server * _get_server(struct cfg *cfg);
 static int  _connect_server(struct connection *c);
 static int  _sockopt(const int fd, int nb);
 static void _close(struct connection *c);
 static void _cleanup(struct cfg *cfg);
 static int  _options(struct cfg *cfg);
 static int  _server(struct cfg *cfg);
-static void _add_server(int signo);
-static void _sigint(int signo);
-static void _stats(int signo);
+static void _sig(int signo);
 static void _version(void);
 static void _usage(void);
 struct cfg *_gcfg = NULL;
@@ -70,20 +68,16 @@ struct cfg *_gcfg = NULL;
 int main(int argc, char *argv[]) {
   struct event ev;
   struct cfg cfg;
-  int r;
 
   if (_cmdline(&cfg, argc, argv) < 0) _usage();
-  _gcfg = &cfg;
 
-  if ( (r = _server(&cfg)) < 0) {
-    if (r == -1) fprintf(stderr, "server: %s\n", strerror(errno));
-    exit(r);
+  if ( (cfg.fd = _server(&cfg)) < 0 || _options(&cfg) < 0) {
+    if (cfg.fd == -1) fprintf(stderr, "server: %s\n", strerror(errno));
+    _cleanup(&cfg); exit(-1);
   }
 
-  if (_options(&cfg) < 0) _exit(-1);
-  memset(&ev, 0, sizeof(struct event));
-  signal(SIGHUP, _stats); signal(SIGUSR1, _add_server); 
-  signal(SIGINT, _sigint); signal(SIGTERM, _sigint); signal(SIGQUIT, _sigint);
+  memset(&ev, 0, sizeof(ev)); _gcfg = &cfg;
+  signal(SIGINT, _sig); signal(SIGTERM, _sig); signal(SIGQUIT, _sig);
 
   event_init();
   event_set(&ev, cfg.fd, EV_READ | EV_PERSIST, _client, &cfg);
@@ -92,35 +86,9 @@ int main(int argc, char *argv[]) {
 }
 
 static void
-_stats(int signo)
+_sig(int signo)
 {
-  int i;
-  size_t sl;
-  time_t now;
-  char h[64], s[64];
-  struct sockaddr *sa;
-  if (!_gcfg || _gcfg->daemon) return;
-  now = time(NULL);
-  printf("%s", ctime(&now));
-  for (i = 0; i < _gcfg->si; i++) {
-    printf("pid=%u tot=%llu", _gcfg->pid, _gcfg->servers[i].tot);
-    sa = _gcfg->servers[i].ai->ai_addr; sl = _gcfg->servers[i].ai->ai_addrlen;
-    if (!getnameinfo(sa, sl, h, sizeof(h), s, sizeof(s), NI_NUMERICHOST | NI_NUMERICSERV)) printf(" server=%s:%s", h, s);
-    printf("\n");
-  }
-  printf("--\n"); fflush(stdout); 
-}
-
-static void
-_add_server(int signo)
-{
-  _parse_server(_gcfg, getenv("RLB_SERVER"));
-}
-
-static void
-_sigint(int signo)
-{
-  _cleanup(_gcfg); free(_gcfg->conn); exit(0);
+  _cleanup(_gcfg); exit(0);
 }
 
 static void
@@ -129,10 +97,12 @@ _cleanup(struct cfg *cfg)
   int i;
   for (i = 0; i < cfg->max; i++) {
     _close(&cfg->conn[i]);
-    free(cfg->conn[i].rb); free(cfg->conn[i].wb);
+    if (cfg->conn[i].rb) free(cfg->conn[i].rb); 
+    if (cfg->conn[i].wb) free(cfg->conn[i].wb);
   }
   for (i = 0; i < cfg->si; i++) freeaddrinfo(cfg->servers[i].ai);
-  free(cfg->servers); cfg->servers = NULL;
+  if (cfg->servers) free(cfg->servers); cfg->servers = NULL;
+  if (cfg->conn) free(cfg->conn); cfg->conn = NULL;
 }
 
 static void
@@ -140,9 +110,9 @@ _read(const int fd, short event, void *c)
 {
   struct connection *cn = c;
   struct cfg *cfg = cn->cfg;
-  unsigned int bs = cfg->bufsize;
   size_t *l = NULL, o = 0;
   struct event *e = NULL;
+  int bs = cfg->bufsize;
   char *b = NULL;
   ssize_t r = 0;
 
@@ -189,28 +159,17 @@ static void
 _close(struct connection *c)
 {
   c->rlen = c->rpos = c->wlen = c->wpos = 0;
-
   if (c->server) c->server->num--;
-
   if (EVENT_FD((&c->c_rev)) >= 0) { event_del(&c->c_rev); c->c_rev.ev_fd = -1; }
   if (EVENT_FD((&c->c_wev)) >= 0) { event_del(&c->c_wev); c->c_wev.ev_fd = -1; }
   if (EVENT_FD((&c->s_rev)) >= 0) { event_del(&c->s_rev); c->s_rev.ev_fd = -1; }
   if (EVENT_FD((&c->s_wev)) >= 0) { event_del(&c->s_wev); c->s_wev.ev_fd = -1; }
-
-#ifdef TCP_CORK___
-  {
-    int on = 0;
-    setsockopt(c->s, IPPROTO_TCP, TCP_CORK, &on, sizeof(on));
-    setsockopt(c->c, IPPROTO_TCP, TCP_CORK, &on, sizeof(on));
-  }
-#endif
-
   if (c->s >= 0) { shutdown(c->s, 2); close(c->s); c->s = -1; }
   if (c->c >= 0) { shutdown(c->c, 2); close(c->c); c->c = -1; }
 }
 
 static struct server *
-_get_server(struct cfg *cfg)
+_get_server(struct cfg *cfg, struct connection *c)
 {
   struct server *s = NULL;
   int i = cfg->cs;
@@ -224,7 +183,6 @@ _get_server(struct cfg *cfg)
       else { if (!now) now = time(NULL); if (now - s->last >= cfg->check) _check_server(cfg, s); }
     }
     if (!s->status || (s->max && s->num + 1 > s->max) ) s = NULL;
-    else break;
   } while (!s && cfg->cs != i);
   return s;
 }
@@ -236,7 +194,7 @@ _connect_server(struct connection *c)
   struct addrinfo *a;
   int fd = -1, r;
 
-  while ( (c->server = _get_server(cfg)) ) {
+  while ( (c->server = _get_server(cfg, c)) ) {
     if ( !(a = c->server->ai) ) return -1;
     if ( (fd = socket(a->ai_family, a->ai_socktype, a->ai_protocol)) < 0) return -1;
     if (_sockopt(fd, 1) < 0) { close(fd); return -1; }
@@ -247,7 +205,7 @@ _connect_server(struct connection *c)
   }
   if (!c->server || fd < 0) return -1;
 
-  c->s = fd; c->server->num++; c->server->tot++;
+  c->s = fd; c->server->num++;
   event_set(&c->s_rev, fd, EV_READ, _read, c);
   event_add(&c->s_rev, &cfg->to);
   return 0;
@@ -311,12 +269,6 @@ _sockopt(const int fd, int nb)
 #ifdef SO_REUSEADDR
   ret |= setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 #endif
-#ifdef TCP_NODELAY___
-  ret |= setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &on, sizeof(on));
-#endif
-#ifdef TCP_CORK___
-  ret |= setsockopt(fd, IPPROTO_TCP, TCP_CORK, &on, sizeof(on));
-#endif
 
   if (nb) ret |= fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
   return ret;
@@ -340,13 +292,11 @@ _server(struct cfg *cfg)
   if ( (fd = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol)) < 0 ||
         _sockopt(fd, 1) < 0 ||
         bind(fd, ai->ai_addr, ai->ai_addrlen) < 0) {
-    if (fd >= 0) close(fd);
-    freeaddrinfo(ai);
+    if (fd >= 0) close(fd); freeaddrinfo(ai);
     return -1;
   }
 
   freeaddrinfo(ai);
-  cfg->fd = fd;
   return fd;
 }
 
@@ -356,18 +306,14 @@ _options(struct cfg *cfg)
   int i = 0;
   unsigned int l = sizeof(cfg->bufsize);
 
-  if (!cfg->si) _usage();
-  if (cfg->fd < 0) return -1;
-
   if (cfg->check == 0) cfg->check = _CHECK;
   if (cfg->to.tv_sec <= 0) cfg->to.tv_sec = _TIMEOUT;   /* XXX Timeout handling */
-  cfg->to.tv_usec = 0;
 
   if (!cfg->bufsize) getsockopt(cfg->fd, SOL_SOCKET, SO_SNDBUF, &cfg->bufsize, &l);
   if (!cfg->bufsize) cfg->bufsize = _BUFSIZE;
 
   if (cfg->daemon) {
-    for (i = 0; i < 3; i++) close(i);
+    close(0); close(1); close(2);
     if (fork()) _exit(0); setsid(); if (fork()) _exit(0);
 
     if (cfg->num) {
@@ -385,7 +331,6 @@ _options(struct cfg *cfg)
     cfg->conn[i].cfg = cfg;
   }
 
-  cfg->pid = getpid();
   if (listen(cfg->fd, SOMAXCONN) < 0) { close(cfg->fd); return -1; }
   return 0;
 }
@@ -400,14 +345,11 @@ _parse_server(struct cfg *cfg, char *str)
   if (!*(cp + 1)) return -1; *cp = 0;
   if ( (p = strchr(cp + 1, ':')) ) *p = 0;
 
-  if ( !(sv = (struct server *) realloc(cfg->servers, (cfg->si + 1) * sizeof(struct server))) ) return -1;
-  cfg->servers = sv;
-  s = &sv[cfg->si];
-  memset(s, 0, sizeof(struct server));
-  if ( !(s->ai = _get_addrinfo(cfg, str, cp + 1)) ) return -1;
-  cfg->si++;
+  if ( !(sv = realloc(cfg->servers, (cfg->si + 1) * sizeof(struct server))) ) return -1;
+  cfg->servers = sv; s = &sv[cfg->si]; memset(s, 0, sizeof(struct server));
+  if ( !(s->ai = _get_addrinfo(cfg, str, cp + 1)) ) return -1; cfg->si++;
 
-  if (p) { if (*(p + 1)) s->max = atoi(p + 1); *p = ':'; } *cp = ':';
+  if (p) { *p++ = ':'; if (*p) s->max = atoi(p); } *cp = ':';
   _check_server(cfg, s);
   return 0;
 }
@@ -426,7 +368,7 @@ _lookup_oaddr(struct cfg *cfg, char *outb)
 struct addrinfo *
 _get_addrinfo(struct cfg *cfg, char *h, char *p)
 {
-  struct addrinfo hints, *res;
+  struct addrinfo hints, *res = NULL;
   int rv;
 
   memset(&hints, 0, sizeof(hints));
@@ -437,6 +379,7 @@ _get_addrinfo(struct cfg *cfg, char *h, char *p)
 
   if ( (rv = getaddrinfo(*h ? h : NULL, p, &hints, &res)) ) { 
     fprintf(stderr, "%s - %s\n", *h ? h : "", gai_strerror(rv)); 
+    if (res) freeaddrinfo(res);
     return NULL; 
   }
   return res;
@@ -446,7 +389,6 @@ static int
 _cmdline(struct cfg *cfg, int argc, char *argv[])
 {
   int i, j;
-  if (!cfg) return -1;
   memset(cfg, 0, sizeof(struct cfg)); cfg->daemon = 1;
   for (i = j = 1; i < argc; j = ++i) {
     if (argv[i][0] != '-') return -1;
@@ -467,6 +409,7 @@ _cmdline(struct cfg *cfg, int argc, char *argv[])
       default : return -1;
     }
   }
+  if (!cfg->si) return -1;
   return 0;
 }
 
