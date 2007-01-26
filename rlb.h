b6424eb68ba00030913d8a1a9258ea906519e4cc
Jason Armstrong <ja@riverdrums.com>
2007-01-26 22:57:28 +0000

    Defines moved to header file. Header file comments. README updated to include buffer information.

--- rlb.h
+++ rlb.h
@@ -21,7 +21,21 @@
 #include <netinet/in.h>
 #include <sys/resource.h>
 #include <event.h>
-#include <assert.h>
+
+#define RLB_VERSION    "0.6"
+#define RLB_TIMEOUT    30        /**< Socket timeout and dead server check interval */
+#define RLB_BUFSIZE    4096      /**< Default buffer size and number of clients to track */
+
+#ifdef RLB_DEBUG
+FILE *_rlb_fp = NULL;
+# define RLOG(f,...) do { if (_rlb_fp) { struct timeval tv; gettimeofday(&tv, NULL); fprintf(_rlb_fp, "%lu.%06lu [%7s:%d] " f "\n", tv.tv_sec, tv.tv_usec, __FUNCTION__, __LINE__, ##__VA_ARGS__); fflush(_rlb_fp); } } while(0)
+# define SCOPE  (c->scope == RLB_CLIENT) ? "CLIENT" : (c->scope == RLB_SERVER) ? "SERVER" : " NONE "
+#else
+# define RLOG(f,...)
+# define SCOPE ""
+#endif
+
+typedef enum { RLB_NONE, RLB_CLIENT, RLB_SERVER } rlb_scope;
 
 struct client {
   unsigned int id;          /**< Client IP address */
@@ -35,8 +49,6 @@ struct server {
   time_t last;              /**< Time of last failed connection */
 };
 
-typedef enum { RLB_NONE, RLB_CLIENT, RLB_SERVER } rlb_scope;
-
 struct buffer {
   char *b;                  /**< Data buffer */
   int bs, taken;            /**< Buffer size / whether it is attached */
@@ -48,7 +60,7 @@ struct connection {
   struct buffer *rb, *wb;   /**< Data buffers for read and write */
   unsigned int nr, nw;      /**< Read and write totals for connection */
   struct event ev;          /**< Event structure for libevent */
-  struct server *server;    /**< RLB_CLIENT only: which server is backend */
+  struct server *server;    /**< CLIENT: which server is backend SERVER: myself */
   struct client *client;    /**< Pointer to previous connection information */
   struct cfg *cfg;          /**< Pointer to global configuration structure */
   rlb_scope scope;          /**< CLIENT=outside connection SERVER=backend server */
@@ -66,7 +78,7 @@ struct connection {
 struct filter {
   void *h;                                      /**< Handle to shared object */
   void *userdata;                               /**< Persistent while rlb is running */
-  int  (*fl)(struct connection *, int, void *); /**< Filter               rlb_filter() */
+  int  (*fl)(struct connection *, int, void *); /**< Filter after read    rlb_filter() */
   void (*cl)(struct connection *, void *);      /**< Connection close     rlb_close() */
   void (*gs)(struct connection *, void *);      /**< Custom choose server rlb_get_server() */
   int  (*in)(struct cfg *, void **);            /**< Global init          rlb_init() */
@@ -77,17 +89,17 @@ struct filter {
 struct cfg {
   int bufsize, num, daemon, fd, check, max; 
   int si, cs, ci, rr, stubborn, delay;
-  struct connection *conn;  /**< Array of 'max' connections */
-  struct buffer *buffers;   /**< Array of 'max' buffers */
-  struct server *servers;   /**< Array of 'si' servers */
-  struct client *clients;   /**< Array of 'ci' clients */
-  char *jail, *user;        /**< chroot() jail / Run as 'user' */
-  struct timeval to;        /**< Timeout value (seconds) */
-  char host[64], port[8];   /**< Listen host and port */
-  struct sockaddr oaddr;    /**< Bind to this address on 'connect()' */
+  struct connection *conn;                      /**< Array of 'max' connections */
+  struct buffer *buffers;                       /**< Array of 'max' buffers */
+  struct server *servers;                       /**< Array of 'si' servers */
+  struct client *clients;                       /**< Array of 'ci' clients */
+  char *jail, *user;                            /**< chroot() jail / Run as 'user' */
+  struct timeval to;                            /**< Timeout value (seconds) */
+  char host[64], port[8];                       /**< Listen host and port */
+  struct sockaddr oaddr;                        /**< Bind to this address on 'connect()' */
   size_t olen;
 #ifdef RLB_SO
-  struct filter *filters;   /**< Array of 'fi' filters */
+  struct filter *filters;                       /**< Array of 'fi' filters */
   int fi, ini, fri, gsi, fli, cli, cf;
 #endif
 };
